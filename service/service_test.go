60650607c4d706bfe9616f64c10ec16d9a4ab30d
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2024-07-01 07:46:29 -0300

    test: still not passing tests

--- service/service_test.go
+++ service/service_test.go
@@ -5,20 +5,17 @@ import (
 
 	"github.com/diegodario88/sesamo/service"
 	"github.com/diegodario88/sesamo/store"
-	"github.com/diegodario88/sesamo/types"
 	"github.com/stretchr/testify/suite"
 )
 
 type ServiceTestSuite struct {
 	suite.Suite
 	mockUserStore *store.MockUserStore
-	mockUser      *types.MockUser
 	userService   service.UserService
 }
 
 func (serviceTestSuite *ServiceTestSuite) SetupTest() {
 	serviceTestSuite.mockUserStore = new(store.MockUserStore)
-	serviceTestSuite.mockUser = new(types.MockUser)
 	serviceTestSuite.userService = service.UserService{
 		Store: serviceTestSuite.mockUserStore,
 	}
--- service/user_service_test.go
+++ service/user_service_test.go
@@ -1,20 +1,22 @@
 package service_test
 
+import "github.com/diegodario88/sesamo/types"
+
 func (suite *ServiceTestSuite) TestAuthenticateUserByEmailPassword() {
 	email := "test@example.com"
 	password := "password123"
-	hashedPassword := "hashedpassword123"
+	hashedPassword := "$argon2id$v=19$m=65536,t=3,p=2$Woo1mErn1s7AHf96ewQ8Uw$D4TzIwGO4XD2buk96qAP+Ed2baMo/KbTRMqXX00wtsU"
 
-	suite.mockUser.Email = email
-	suite.mockUser.PasswordHash = &hashedPassword
+	correctUser := &types.User{
+		Email:        email,
+		PasswordHash: &hashedPassword,
+	}
 
-	suite.mockUserStore.On("FindUserByEmail", email).Return(suite.mockUser, nil)
-	suite.mockUser.On("CheckPassword", password).Return(true, nil)
+	suite.mockUserStore.On("FindUserByEmail", email).Return(correctUser, nil)
 
 	result, err := suite.userService.AuthenticateUserByEmailPassword(email, password)
 
 	suite.NoError(err)
-	suite.Equal(suite.mockUser, result)
+	suite.NotNil(result)
 	suite.mockUserStore.AssertExpectations(suite.T())
-	suite.mockUser.AssertExpectations(suite.T())
 }
