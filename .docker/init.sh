6d20c3c35788cd48808b171a353b6616c9bd2283
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2025-03-22 08:24:35 -0300

    chore: update configuration and database initialization scripts
    Updated the .air.toml configuration file to modify the exclusion
    regex key and adjust the kill delay to 60 seconds. Enabled sending
    interrupts and stopping on errors to improve process handling.
    
    In the .docker/init.sh script, added the creation of the "pg_cron"
    extension for the sesamo_test database and included the "hstore"
    extension for all databases. These changes enhance database
    functionality and ensure necessary extensions are available.

--- .docker/init.sh
+++ .docker/init.sh
@@ -4,10 +4,11 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   DROP DATABASE IF EXISTS sesamo_test;
   CREATE DATABASE sesamo_test;
+  CREATE EXTENSION IF NOT EXISTS "pg_cron";
 EOSQL
 
 for DB in $(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -t -c "SELECT datname FROM pg_database WHERE datname NOT IN ('postgres', 'template0', 'template1')"); do
     echo $DB
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -d $DB -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; CREATE EXTENSION IF NOT EXISTS "citext";'
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -d $DB -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; CREATE EXTENSION IF NOT EXISTS "citext";CREATE EXTENSION IF NOT EXISTS "hstore";'
 done
 
