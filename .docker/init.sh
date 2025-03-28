53aa45bdf23c067d37f195f2a2411565f6ffb50c
Diego Dario <25825145+diegodario88@users.noreply.github.com>
2025-03-28 11:20:35 -0300

    feat: enhance database initialization script and update .gitignore
    Enhances the database initialization script by adding more detailed
    logging and checks for extension availability. It now includes
    initialization messages, lists databases, and verifies extensions
    after creation. The script also checks for the 'pgx_ulid' extension
    availability and creates the 'ulid' extension if not present. The
    .gitignore file is updated to include the '.env' file, preventing
    environment configuration files from being tracked in the repository.

--- .docker/init.sh
+++ .docker/init.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 set -e
 
+echo "Initializing databases..."
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   DROP DATABASE IF EXISTS sesamo_test;
   CREATE DATABASE sesamo_test;
   CREATE EXTENSION IF NOT EXISTS "pg_cron";
 EOSQL
 
-for DB in $(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -t -c "SELECT datname FROM pg_database WHERE datname NOT IN ('postgres', 'template0', 'template1')"); do
-    echo $DB
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -d $DB -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; CREATE EXTENSION IF NOT EXISTS "citext";CREATE EXTENSION IF NOT EXISTS "hstore";'
+echo "Listing databases..."
+DATABASES=$(psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -t -c "SELECT datname FROM pg_database WHERE datname NOT IN ('postgres', 'template0', 'template1')")
+
+for DB in $DATABASES; do
+    echo "Processing database: $DB"
+
+    echo "Checking pgx_ulid extension availability..."
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB" -c '\dx pgx_ulid'
+
+    echo "Creating extensions in $DB..."
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB" -c '
+        CREATE EXTENSION IF NOT EXISTS "ulid";
+        CREATE EXTENSION IF NOT EXISTS "citext";
+        CREATE EXTENSION IF NOT EXISTS "hstore";
+    '
+
+    echo "Verifying extensions in $DB..."
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB" -c '\dx'
 done
 
+echo "Database initialization complete."
